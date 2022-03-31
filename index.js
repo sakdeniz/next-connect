ec9d22cb56ebfd49f75b50a5defee20ba100f52c
sakdeniz <38104512+sakdeniz@users.noreply.github.com>
2022-03-31 17:16:02 +0300

    Varios updates

--- index.js
+++ index.js
@@ -1,47 +1,224 @@
+var mysql = require('mysql');
+var http=require('http');
+var server;
+var argv=require('minimist')(process.argv.slice(2));
+const config={headers: {'Content-Type': 'application/x-www-form-urlencoded'},responseType: 'text'};
 const ElectrumClient = require('@aguycalled/electrum-client-js')
 
-async function main() {
-  const client = new ElectrumClient(
-    'electrum-testnet.nav.community',
-    40004,
-    'wss'
-  )
+	global.window = global;
+	const setGlobalVars = require("indexeddbshim");
+	setGlobalVars(null, { checkOrigin: false });
 
-  const verifyStatus = (s) => 
-  { if (s[1] && s[1].spender_txhash)
-  	{
-  		invalidateProof(s[0][0], s[0][1]);
-  		console.log("moved");
-		console.log(s);
-  	}
-  }
-  client.subscribe.on(
-        "blockchain.outpoint.subscribe", verifyStatus);
+	const njs = require("navcoin-js");
+	const repl = require("repl");
 
-  try {
-    await client.connect(
-      'electrum-client-js', // optional client name
-      '1.5' // optional protocol version
-    )
-    let txid="2c1d20fe60d6ab296a28e7db9d00efa2b6a3b7df40c270b5bc16be4dd58ba7bc";
-    let vout="0";
-    let currentStatus = await client.blockchain_outpoint_subscribe(
-      txid,
-      vout
-    );
-    verifyStatus([[txid, vout], currentStatus]);
+	const walletFile = undefined; // File name of the wallet database, persistence using dexie db backend only works on the browser
+	const password = undefined; // Password used to encrypt and open the wallet database
+	const spendingPassword = undefined; // Password used to send transactions
+	const mnemonic = "bracket shrug kit web three run stem resist barrel spring bounce clock"
+	const type = undefined; // Wallet type next, navcoin-core or navcoin-js-v1
+	const zapwallettxes = false; // Should the wallet be cleared of its history?
+	const log = true; // Log to console
+	const network = "testnet";
 
-    txid="dc3f313cf37175a0073665f5d1eaddd190d2ea08b648f6ea2659687b333ab740";
-    vout="1";
-    currentStatus = await client.blockchain_outpoint_subscribe(
-      txid,
-      vout
-    );
-    verifyStatus([[txid, vout], currentStatus]);
+	let wallet;
 
-  } catch (err) {
-    console.error(err)
-  }
+	const prompt = repl.start("> ");
+
+async function main()
+{
+	njs.wallet.Init().then(async () => {
+	  wallet = new njs.wallet.WalletFile({
+	    file: walletFile,
+	    mnemonic: mnemonic,
+	    type: type,
+	    password: password,
+	    spendingPassword: spendingPassword,
+	    zapwallettxes: zapwallettxes,
+	    log: log,
+	    network: network,
+	  });
+
+	  prompt.context.wallet = wallet;
+
+	  wallet.on("new_mnemonic", (mnemonic) =>
+	    console.log(`wallet created with mnemonic ${mnemonic} - please back it up!`)
+	  );
+
+	  wallet.on("loaded", async () => {
+	    console.log("wallet loaded");
+
+	    console.log(
+	      "xNAV receiving address: " +
+	        (await wallet.xNavReceivingAddresses(true))[0].address
+	    );
+	    console.log(
+	      "NAV receiving address: " +
+	        (await wallet.NavReceivingAddresses(true))[0].address
+	    );
+
+	    await wallet.Connect();
+	  });
+
+	  wallet.on("connected", () => console.log("connected. waiting for sync"));
+
+	  wallet.on("sync_status", async (progress, pending) => {
+	    console.log(`Sync ${progress}%`);
+	  });
+
+	  wallet.on("db_load_error", async (err) => {
+	    console.log(`Error Load DB: ${err}`);
+	    process.exit(1);
+	  });
+
+	  wallet.on("sync_finished", async () => {
+	    console.log("sync_finished");
+	    console.log(`Balance ${JSON.stringify(await wallet.GetBalance())}`);
+	  });
+
+	  wallet.on("new_tx", async (list) => {
+	    console.log(`Received transaction ${JSON.stringify(list)}`);
+	    console.log(`Balance ${JSON.stringify(await wallet.GetBalance())}`);
+	  });
+
+	  await wallet.Load({
+	    bootstrap: njs.wallet.xNavBootstrap,
+	  });
+	});
+
+
+	const client = new ElectrumClient(
+		'electrum-testnet.nav.community',
+		40004,
+		'wss'
+	)
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: ""
+	});
+
+	con.connect(function(err)
+	{
+		if (err) throw err;
+		console.log("Connected to MySQL!");
+	});
+	function invalidateProof(s)
+	{
+		console.log("NFT Moved");
+		console.log("Old hash -> " + s[0][0]);
+		console.log("New hash -> " + s[1].spender_txhash);
+	}
+	server=http.createServer(function (req, res)
+	{
+		res.setHeader('Access-Control-Allow-Origin', "*");
+		res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+		var url=req.url;
+		var body='';
+		req.on('data', function (data)
+		{
+			body+=data;
+		});
+		req.on('end', function ()
+		{
+			var post=body?JSON.parse(body) : {}
+			var now=new Date(); 
+			var datetime=now.getHours()+':'+now.getMinutes()+':'+now.getSeconds(); 
+			console.log(datetime + " " + req.url);
+			if (req.url=="/proof")
+			{
+				console.log("Proof");
+				console.log(post.proof);
+				console.log("Result");
+				console.log(post.result);
+				//
+				console.log("Verifying NFT -> " + post.proof.tokenId + "(" + post.proof.nftId + ")");
+				let hex=Buffer.from(post.proof.sig).toString('hex');
+				let proof={nftId:parseInt(post.proof.nftId),tokenId:post.proof.tokenId.toString(),sig:Buffer.from(hex,'hex')};
+				wallet.VerifyNftProof(post.proof.tokenId.toString(),parseInt(post.proof.nftId),proof).then((retval) =>
+				{
+					console.log(retval);
+					if (retval.result)
+					{
+						console.log("NFT ownership verified -> " + post.proof.tokenId + "(" + post.proof.nftId + ")");
+						let sql = `INSERT INTO nft.proofs(
+						    id,
+						    private_address,
+						    tokenid,
+						    nft_id,
+						    hash,
+						    new_hash,
+						    verification_date,
+						    is_valid
+						)
+						VALUES(
+						    NULL,
+						    '`+post.proof.privateAddress+`',
+						    '`+post.proof.tokenId.toString()+`',
+						    '`+post.proof.nftId+`',
+						    '`+post.result.txid+`',
+						    NULL,
+						    NOW(),
+						    '1'
+						);`;
+						console.log(sql);
+						con.query(sql, function (err, result)
+						{
+							if (err)
+							{
+								console.log("NFT ownership record not added -> " + err);
+							}
+							else
+							{
+								console.log("NFT ownership record added to database.");
+							}
+						});
+					}
+					else
+					{
+						console.log("NFT ownership verification failed -> " + post.proof.tokenId + "(" + post.proof.nftId + ")");
+					}
+				}).
+				catch((e) =>
+				{
+					console.log("Error while verifying nft proof");
+					console.log(e);
+				});
+			}
+		});
+	});
+
+	const verifyStatus = (s) => 
+	{
+		if (s[1] && s[1].spender_txhash)
+		{
+			invalidateProof(s);
+		}
+	}
+	client.subscribe.on("blockchain.outpoint.subscribe", verifyStatus);
+	try
+	{
+		await client.connect(
+			'electrum-client-js', // optional client name
+			'1.5' // optional protocol version
+		)
+		
+		let txid="2c1d20fe60d6ab296a28e7db9d00efa2b6a3b7df40c270b5bc16be4dd58ba7bc";
+		let vout="0";
+		let currentStatus = await client.blockchain_outpoint_subscribe(txid,vout);
+		verifyStatus([[txid, vout], currentStatus]);
+		
+		txid="dc3f313cf37175a0073665f5d1eaddd190d2ea08b648f6ea2659687b333ab740";
+		vout="1";
+		currentStatus = await client.blockchain_outpoint_subscribe(txid,vout);
+		verifyStatus([[txid, vout], currentStatus]);
+	}
+	catch (err)
+	{
+		console.error(err)
+	}
+	server.listen(argv.p || 3000);
+	console.log('Server running on port ' + (argv.p || 3000))
 }
-
 main();
